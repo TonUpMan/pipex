944e28d76c822a0581c8cdf4e785bb96a9072c8e
tonupman <tonupman@workspace.myguest.virtualbox.org>
2024-01-22 16:11:09 +0100

    create skull of action

--- src/main.c
+++ src/main.c
@@ -11,3 +11,34 @@
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+int main(int argc, char **argv)
+{
+    int pid;
+    int fdchild;
+    int fdparent;
+
+    pid = fork();
+    if (pid == 0)
+    {
+        fdchild = open(argv[1], O_RDONLY); //open  file1
+        execve(argv[2]); // exec cmd 1
+    }
+    else
+    {
+        wait(0); // parent wait child end
+
+    }
+    if (pid == 0)
+    {
+        dup2(fdchild, pipe(fd[0])); // enter pipe 
+        close(fdchild);
+    }
+    else
+    {
+        dup2(pipe(fd[1]), fdparent); // end pipe
+        close(pipe(fd[1])); 
+        execve(argv[3]) //exec cmd 2
+        //write in file 2
+    }
+}
