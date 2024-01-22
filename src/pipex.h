389212fde5cbcf623c10d8399fadc16026ad5a32
Quentin Devianne <qdeviann@2D6.42angouleme.fr>
2024-01-22 12:01:21 +0100

    start pipex project

---
+++ src/pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qdeviann <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 10:50:32 by qdeviann          #+#    #+#             */
+/*   Updated: 2024/01/22 10:50:37 by qdeviann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include "../libft/libft.h"
+
+#endif
