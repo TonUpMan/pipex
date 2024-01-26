15997326699afa65c3d3e15e5600730a71777c90
Quentin Devianne <qdeviann@2D6.42angouleme.fr>
2024-01-26 01:12:14 +0100

    pipe fonctionnel, 2 child

--- src/main.c
+++ src/main.c
@@ -15,20 +15,45 @@
 int main(int argc, char **argv, char **envp)
 {
 	char		**path;
-	char 		**flags;
-	int			i;
+	char 		**option1;
+	char 		**option2;
+	pid_t		pid1;
+	pid_t		pid2;
 	int			fd1;
-	//int			fd2;
+	int			fd2;
+	int			pipefd[2];
 
-	i = 0;
 	check_arg(argc, argv, envp);
-	flags = find_flags(argv[2]);
+	option1 = ft_split(argv[2], ' ');
+	option2 = ft_split(argv[3], ' ');
 	path = find_paths(argc, argv, envp);
-	fd1 = open(argv[1], O_RDONLY);
-	dup2(fd1, 0);
-	close(fd1);
-	execve(path[0], flags, envp);
-
-
-	return (0);
+	if(pipe(pipefd) == -1)
+		ft_printf("erreur pipe!");
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		fd1 = open(argv[1], O_RDONLY);
+		dup2(fd1, 0);
+		close(fd1);
+		close(pipefd[0]);				
+		dup2(pipefd[1], 1);
+		execve(path[0], option1, envp);
+		exit(0);
+	}
+	if (pid1 != 0)	
+	{
+		pid2 = fork();
+		if (pid2 == 0)
+		{
+			close(pipefd[1]);
+			dup2(pipefd[0], 0);
+			fd2 = open(argv[4], O_WRONLY);
+			dup2(fd2, 1);
+			close(fd2);
+			execve(path[1], option2, envp);
+			exit(0);
+		}
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
 }
